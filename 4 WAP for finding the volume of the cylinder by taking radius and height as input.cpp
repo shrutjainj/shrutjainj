3cabdb514a29daa03d6c1c33dadb064cd4bf4066
shrutjainj <163582717+shrutjainj@users.noreply.github.com>
2024-03-17 10:23:02 +0530

    Add files via upload

---
+++ 4 WAP for finding the volume of the cylinder by taking radius and height as input.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int v, r, h;
+    cin>>r;
+    cin>>h;
+    v = 3.14*r*r*h;
+    cout<<"volume of a cylinder is: "<<v;
+}
---
+++ 5 WAP to find the difference between ASCII of two characters ,take them as input.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    char a, b;
+    cin>>a;
+    cin>>b;
+    int c = int(a);
+    int e = int(b);
+    int d = c-e;
+    cout<<d;
+}
